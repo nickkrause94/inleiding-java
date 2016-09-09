597e417911027f8ee06f1f874bb2ffa5f294f650
nickkrause94 <nickkrause94@live.com>
2016-09-09 11:19:16 +0200

    geen verandering

--- src/Hoofdstuk4/Opdracht5.java
+++ src/Hoofdstuk4/Opdracht5.java
@@ -7,7 +7,7 @@ public class Opdracht5 extends Applet{ public void init(){setBackground(Color.bl
     public void paint(Graphics g){
         g.drawOval(20, 20, 120, 80);
         g.setColor(Color.yellow);
-        g.fillArc(20, 20, 120, 80, 90, 70);
+        g.fillArc(20, 20, 120, 80, 90, 45);
     }
 
 
